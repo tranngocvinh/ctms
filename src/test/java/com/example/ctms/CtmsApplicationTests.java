6f3fe9cfa148f75b5fb286d57415c2413df0e30b
tran ngoc vinh <vinhvinh307@gmail.com>
2024-05-21 21:39:14 +0700

    first commit

---
+++ src/test/java/com/example/ctms/CtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ctms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
