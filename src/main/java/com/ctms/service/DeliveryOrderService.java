9f5a908a8a7769b5842e37362af6fb3fed8291cc
tran ngoc vinh <vinhvinh307@gmail.com>
2024-09-25 20:52:52 +0700

    test

--- src/main/java/com/ctms/service/DeliveryOrderService.java
+++ src/main/java/com/ctms/service/DeliveryOrderService.java
@@ -184,6 +184,7 @@ public class DeliveryOrderService {
         deliveryOrderRepository.save(order);
     }
 
+
     public Double getTotalPaidRepairCost() {
         return deliveryOrderRepository.sumPaiDeliveryCost();
     }
